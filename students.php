d26c4b927bb0796c8b4ecde896688f1e7012b9b6
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-10-30 23:49:13 -0500

    Update students.php

--- students.php
+++ students.php
@@ -31,7 +31,46 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+      $sqlAdd = "insert into OUstudents (student_name) value (?)";
+      $stmtAdd = $conn->prepare($sqlAdd);
+      $stmtAdd->bind_param("s", $_POST['iName']);
+      $stmtAdd->execute();
+      echo '<div class="alert alert-success" role="alert">New OUstudents added.</div>';
+      break;
+    case 'Edit':
+      $sqlEdit = "update OUstudents set student_name=? where student_id=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("si", $_POST['iName'], $_POST['iid']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">OUstudents edited.</div>';
+      break;
+    case 'Delete':
+      $sqlDelete = "delete from OUstudents where student_id=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("i", $_POST['iid']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">OUstudents deleted.</div>';
+      break;
+  }
+}
+?>
+    
+      <h1>OU students</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          
+<?php
 $sql = "SELECT student_id, student_name from OUstudents";
 $result = $conn->query($sql);
 
@@ -39,12 +78,46 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <tr>
-    <td><?=$row["student_id"]?></td>
-    <td><?=$row["student_name"]?></td>
-   
-  
-  </tr>
+          
+          <tr>
+            <td><?=$row["student_id"]?></td>
+            <td><a href="OUstudents-section.php?id=<?=$row["student_id"]?>"><?=$row["student_name"]?></a></td>
+            <td>
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editOUstudents<?=$row["student_id"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editOUstudents<?=$row["student_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editOUstudents<?=$row["student_id"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editOUstudents<?=$row["student_id"]?>Label">Edit OUstudents</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editOUstudents<?=$row["student_id"]?>Name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editOUstudents<?=$row["student_id"]?>Name" aria-describedby="editOUstudents<?=$row["student_id"]?>Help" name="iName" value="<?=$row['student_name']?>">
+                          <div id="editOUstudents<?=$row["istudent_id"]?>Help" class="form-text">Enter the OUstudents's name.</div>
+                        </div>
+                        <input type="hidden" name="iid" value="<?=$row['student_id']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="iid" value="<?=$row["student_id"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+              </form>
+            </td>
+          </tr>
+          
 <?php
   }
 } else {
@@ -52,8 +125,38 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </tbody>
-    </table>
+          
+        </tbody>
+      </table>
+      <br />
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addOUstudents">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addOUstudents" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addOUstudentsLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addOUstudentsLabel">Add OUstudents</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="OUstudentsName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="OUstudentsName" aria-describedby="nameHelp" name="iName">
+                  <div id="nameHelp" class="form-text">Enter the OUstudents's name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
