b4527c89c518620f7a1aec4aee6236f7a0abf30e
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-15 16:16:30 -0600

    Update section-edit-save.php

--- section-edit-save.php
+++ section-edit-save.php
@@ -35,7 +35,7 @@ $sql = "update section set section_number=? where section_id=?";
 <div class="alert alert-success" role="alert">
   Instructor edited.
 </div>
-    <a href="instructors.php" class="btn btn-primary">Go back</a>
+    <a href="sections.php" class="btn btn-primary">Go back</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
